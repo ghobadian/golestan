4665160e21647638cd09dea30fc66771285f1c8f
MohammadSadeghGhobadian <ghobadian80@gmail.com>
2022-08-03 13:34:11 +0430

    more repository related methods added

--- src/main/java/tech/sobhan/golestan/repositories/RepositoryHandler.java
+++ src/main/java/tech/sobhan/golestan/repositories/RepositoryHandler.java
@@ -2,7 +2,7 @@ package tech.sobhan.golestan.repositories;
 
 import lombok.Getter;
 import org.springframework.stereotype.Component;
-import tech.sobhan.golestan.auth.User;
+import tech.sobhan.golestan.models.users.User;
 import tech.sobhan.golestan.business.exceptions.*;
 import tech.sobhan.golestan.models.Course;
 import tech.sobhan.golestan.models.CourseSection;
@@ -11,11 +11,14 @@ import tech.sobhan.golestan.models.Term;
 import tech.sobhan.golestan.models.users.Instructor;
 import tech.sobhan.golestan.models.users.Student;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Component
 @Getter
-public final class RepositoryHandler {
+public class RepositoryHandler {
     private final UserRepository userRepository;
     private final StudentRepository studentRepository;
     private final CourseRepository courseRepository;
@@ -89,8 +92,8 @@ public final class RepositoryHandler {
         courseSectionRepository.delete(specifiedCourseSection);
     }
 
-    public void saveStudent(Student student) {
-        studentRepository.save(student);
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
     }
 
     public Instructor saveInstructor(Instructor instructor) {
@@ -114,5 +117,118 @@ public final class RepositoryHandler {
 
     public List<CourseSection> findCourseSectionByInstructor(Long id) {
         return courseSectionRepository.findByInstructor(id);
+    }
+
+    public void deleteInstructor(Instructor instructor) {
+        instructorRepository.delete(instructor);
+    }
+
+    public Student findStudent(Long id) {
+        return studentRepository.findById(id).orElseThrow(StudentNotFoundException::new);
+    }
+
+    public CourseSectionRegistration findCourseSectionRegistrationByCourseSectionAndStudent(Long courseSectionId, Long studentId) {
+        return courseSectionRegistrationRepository
+                .findByCourseSectionAndStudent(courseSectionId, studentId)
+                .orElseThrow(CourseSectionRegistrationNotFoundException::new);
+    }
+
+    public List<Student> findAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public Student findStudentByUsername(String username) {
+        User user = userRepository.findByUsername(username).orElseThrow(UserNotFoundException::new);
+        return user.getStudent();
+    }
+
+    public List<CourseSectionRegistration> findCourseSectionRegistrationByStudent(Long studentId) {
+        return courseSectionRegistrationRepository.findByStudent(studentId);
+    }
+
+    public User findUserByUsername(String username) {
+        return userRepository.findByUsername(username).orElseThrow(UserNotFoundException::new);
+    }
+
+    public List<Term> findAllTerms() {
+        return termRepository.findAll();
+    }
+
+    public User findUserByStudent(Long id) {
+        return userRepository.findByStudentId(id).orElseThrow(UserNotFoundException::new);
+    }
+
+    public User findUserByInstructor(Long id) {
+        return userRepository.findByInstructorId(id).orElseThrow(UserNotFoundException::new);
+    }
+
+    public Term saveTerm(Term term) {
+        return termRepository.save(term);
+    }
+
+    public void deleteTerm(Term term) {
+        termRepository.delete(term);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<Course> findAllCourses() {
+        return courseRepository.findAll();
+    }
+
+    public List<CourseSection> findAllCourseSections(){
+        return courseSectionRepository.findAll();
+    }
+
+    public Course saveCourse(Course course) {
+        return courseRepository.save(course);
+    }
+
+    public void deleteCourse(Course course) {
+        courseRepository.delete(course);
+    }
+
+    public int findNumberOfStudentsInCourseSection(Long id) {
+        return courseSectionRegistrationRepository.findNumberOfStudents(id);
+    }
+
+    public List<CourseSection> findCourseSectionByInstructorName(String instructorName) {
+        List<CourseSection> allCourseSections = findAllCourseSections();
+        List<CourseSection> output = new ArrayList<>();
+        for (CourseSection courseSection : allCourseSections) {
+            Long currentCourseSectionInstructorId = courseSection.getInstructor().getId();
+            String currentCourseSectionInstructorName = findUserByInstructor(currentCourseSectionInstructorId).getName();
+            if(instructorName.equals(currentCourseSectionInstructorName)){
+                output.add(courseSection);
+            }
+        }
+        return output;
+    }
+
+    public List<CourseSection> findCourseSectionByCourseName(String courseTitle) {
+        return courseSectionRepository.findByCourseTitle(courseTitle);
+    }
+
+    public List<CourseSection> findCourseSectionByInstructorNameAndCourseName(String instructorName, String courseName) {
+        Set<CourseSection> courseSectionsByInstructorName = new HashSet<>(findCourseSectionByCourseName(courseName));
+        Set<CourseSection> courseSectionsByCourseName = new HashSet<>(findCourseSectionByInstructorName(instructorName));
+        courseSectionsByCourseName.retainAll(courseSectionsByInstructorName);
+        return new ArrayList<>(courseSectionsByCourseName);
+    }
+
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public void deleteAll() {
+        courseSectionRegistrationRepository.deleteAll();
+        courseSectionRepository.deleteAll();
+        courseRepository.deleteAll();
+        termRepository.deleteAll();
+        userRepository.deleteAll();
+        studentRepository.deleteAll();
+        instructorRepository.deleteAll();
     }
 }
