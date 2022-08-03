58bd97a9843f710368b9c1861fdbbcd81e4756ca
MohammadSadeghGhobadian <ghobadian80@gmail.com>
2022-08-03 13:24:42 +0430

    extra queries added

--- src/main/java/tech/sobhan/golestan/repositories/UserRepository.java
+++ src/main/java/tech/sobhan/golestan/repositories/UserRepository.java
@@ -3,8 +3,9 @@ package tech.sobhan.golestan.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import tech.sobhan.golestan.auth.User;
+import tech.sobhan.golestan.models.users.User;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +16,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query("SELECT u FROM User u WHERE u.student.studentId = ?1")
     Optional<User> findByStudentId(Long studentId);
 
+    @Query("SELECT u FROM User u WHERE u.name = ?1")
+    List<User> findByName(String name);
+
+    @Query("SELECT u FROM User u WHERE u.instructor.id = ?1")
+    Optional<User> findByInstructorId(Long id);
 }
